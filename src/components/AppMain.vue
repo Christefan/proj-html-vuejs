6048dda500b23fa31680d18fac9b2ce256775e2c
Christefan <chriscapod7@gmail.com>
2022-05-31 12:57:59 +0200

    Main incompleto (4/6)

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,6 @@
 <template>
   <main>
-    <div class="jumbotron">
+    <section class="jumbotron">
       <div class="container d-flex align-items-center">
         <div class="row-cols-12 text-center">
           <h1>Contemporary Ideas</h1>
@@ -13,8 +13,8 @@
           <button class="button-c mt-3">REGISTER NOW</button>
         </div>
       </div>
-    </div>
-    <div class="selection-application py-5">
+    </section>
+    <section class="selection-application py-5">
       <div class="container-fluid">
         <div class="row text-center text-wrap justify-content-center">
           <div class="container-application mx-2 py-5">
@@ -43,7 +43,7 @@
           </div>
         </div>
       </div>
-    </div>
+    </section>
     <section class="list-scroll-image">
       <div class="text px-4">
         <h2 class="fw-bold">The Trusted Name for In-Home Tutoring.</h2>
@@ -55,12 +55,65 @@
         </p>
         <a href="">Learn More</a>
       </div>
-      <div class="image">
-        <img src="../assets/img/h5-img-1.jpg" alt="" />
-        <div class="button-t">
-          <p>></p>
-          <a href="">TOP</a>
+      <!-- Correggere row e colonne -->
+      <div class="container-fluid">
+        <div class="image row align-items-center px-3">
+          <div class="col-11">
+            <img src="../assets/img/h5-img-1.jpg" alt="" />
+          </div>
+          <div class="button-t col-1">
+            <p>></p>
+            <a class="fw-bold fs-5" href="">TOP</a>
+          </div>
         </div>
+      </div>
+    </section>
+    <section class="list-scroll-image-2">
+      <div class="container-fluid">
+        <div class="row align-items-center">
+          <div class="col-6">
+            <img src="../assets/img/h5-img-2.jpg" alt="" />
+          </div>
+          <div class="col-5">
+            <h2 class="fw-bold">
+              Empowering Childrento to Reach Their Potential
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Reiciendis, voluptatem illo vero porro odit rem voluptates, omnis
+              libero molestiae voluptatibus fugiat sunt explicabo fuga facere
+              magnam rerum, officia ipsa cumque!
+            </p>
+            <div class="text row align-items-center">
+              <div class="col-6 px-3">
+                <span>168</span>
+                <p class="px-2 fw-bold">User Stories</p>
+              </div>
+              <div class="col-6">
+                <span>347</span>
+                <p class="px-2 fw-bold">Events</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-1 button-t">
+            <p>></p>
+            <a class="fw-bold fs-5" href="">TOP</a>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="list-scroll-image-3 my-5">
+      <div class="container text-center">
+        <h2>Prova</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia repellat
+          eum ipsum minima molestiae nesciunt, aliquam quaerat voluptates
+          deleniti nulla dolores itaque consequatur debitis provident labore?
+          Veritatis totam minima illo.
+        </p>
+
+        <h2>Joan Collins</h2>
+        <p>STUDENT</p>
       </div>
     </section>
   </main>
@@ -107,9 +160,11 @@ main {
       width: calc(90% / 6);
       background-color: #f2f8fc;
     }
+    img {
+      width: 38%;
+    }
   }
   .list-scroll-image {
-    height: 80vh;
     // background-color: #40c4ff;
     .text {
       h2 {
@@ -120,20 +175,40 @@ main {
       }
     }
   }
-  .image {
-    img {
-      width: 90%;
+  .list-scroll-image-2 {
+    // background-color: #40c4ff;
+    h2 {
+      font-size: 70px;
     }
-    .button-t {
-      background-color: #40c4ff;
-      border: none;
-      color: white;
-      padding: 15px 32px;
-      text-align: center;
-      text-decoration: none;
-      display: inline-block;
-      font-size: 16px;
+    p {
+      font-size: 30px;
     }
+    .text {
+      color: #40c4ff;
+      span {
+        font-size: 70px;
+      }
+      p {
+        font-size: 25px;
+      }
+    }
+  }
+  img {
+    width: 100%;
+  }
+  .button-t {
+    background-color: #40c4ff;
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+  }
+  .list-scroll-image-3 {
+    height: 90vh;
+    background-image: url("../assets/img/h5-parallax-img-1.png");
   }
 }
 </style>
